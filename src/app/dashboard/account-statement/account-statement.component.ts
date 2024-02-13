73796aeb93002235932716a1024bc6b287e0b225
AlineFernandaDev <alinefds.vieira@gmail.com>
2024-02-13 13:45:57 -0300

    [Replace Default Structure] Replace default project structure with custom structure
    This commit replaces the default project structure with a custom structure tailored to our project's specific requirements.
    
    Changes made:
    - Reorganized project directories and files for improved organization and maintainability
    - Added custom configuration files and settings.

---
+++ src/app/dashboard/account-statement/account-statement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-statement',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-statement.component.html',
+  styleUrl: './account-statement.component.scss'
+})
+export class AccountStatementComponent {
+
+}
