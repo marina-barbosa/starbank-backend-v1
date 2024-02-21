7085d91bb88fd97468b91604d1a9b6831fb27735
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-02-21 18:54:56 -0300

    feat: landing page incompleta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from "./dashboard/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, FormsModule, RouterOutlet, HomeComponent, FooterComponent]
+    imports: [CommonModule, FormsModule, RouterOutlet, HomeComponent, FooterComponent, LandingPageComponent]
 })
 export class AppComponent {
   title = 'StarPay';
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+
+//   document.addEventListener('DOMContentLoaded', function () {
+//     const accordionTitles = document.querySelectorAll('.accordion-title');
+
+//     accordionTitles.forEach(title => {
+//         title.addEventListener('click', function () {
+//             const content = this.nextElementSibling;
+//             content.classList.toggle('active');
+
+//             if (content.classList.contains('active')) {
+//                 content.style.display = 'block';
+//             } else {
+//                 content.style.display = 'none';
+//             }
+//         });
+//     });
+// });
+
+
+
+
+
+}
