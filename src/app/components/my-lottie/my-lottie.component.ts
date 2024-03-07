020fab93d414332ff89045813fb3087a14c57fd9
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-03-07 20:12:35 -0300

    feat: cria my-lottie

---
+++ src/app/components/my-lottie/my-lottie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-lottie',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-lottie.component.html',
+  styleUrl: './my-lottie.component.scss'
+})
+export class MyLottieComponent {
+
+}
