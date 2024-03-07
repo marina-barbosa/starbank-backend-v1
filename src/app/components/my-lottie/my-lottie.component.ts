136593f9211ba1f635b8766e7f5a98eb650e9e99
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-03-07 20:19:59 -0300

    feat: att lottie

--- src/app/components/my-lottie/my-lottie.component.ts
+++ src/app/components/my-lottie/my-lottie.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AnimationItem } from 'lottie-web';
+import { LottieDirective, AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { timer } from 'rxjs/internal/observable/timer';
 
 @Component({
   selector: 'app-my-lottie',
   standalone: true,
-  imports: [],
+  imports: [LottieDirective, LottieComponent, RouterLink],
   templateUrl: './my-lottie.component.html',
   styleUrl: './my-lottie.component.scss'
 })
-export class MyLottieComponent {
+export class MyLottieComponent implements OnInit {
+  options: AnimationOptions = {
+    path: '/assets/lottie.json',
+  };
+
+  animationFinished = false;
+
+  constructor(private router: Router) {} // Injetar Router
+
+  ngOnInit() {
+    // ... (código opcional)
+  }
+
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+    timer(3500).subscribe(() => this.router.navigate(['/home']));
+  }
 
 }
