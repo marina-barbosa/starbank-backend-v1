f09533e8299b9cb2f64a7149d95f17184ab0d36d
Aline Fernanda <alinefds.vieira@gmail.com>
2024-03-08 17:02:50 -0300

    Revert "Revert "Lottie""

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './dashboard/home/home.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { MyLottieComponent } from './components/my-lottie/my-lottie.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'land', component: LandingPageComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'lottie', component: MyLottieComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'land' }
 ];
