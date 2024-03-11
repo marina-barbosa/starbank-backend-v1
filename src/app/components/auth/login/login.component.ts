502c4311b76243586369d2d407d7b80cf998294d
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-03-11 17:15:20 -0300

    style: login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../../../services/user.service';
-import { User } from '../../../Models/User';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
+// export class LoginComponent implements OnInit  {
+
+//   loginForm: FormGroup;
+//   errorMessage: string | undefined;
+//   showTour: boolean = true;
+
+//   constructor(private router: Router) {
+//     this.loginForm = new FormGroup({
+//       username: new FormControl('', [Validators.required]),
+//       password: new FormControl('', [Validators.required]),
+//     });
+//   }
+
+//   ngOnInit(): void {
+//   }
+
+//   onSubmit() {
+//     if (this.loginForm.valid) {
+//       const { username, password } = this.loginForm.value;
+
+//       if (username === 'admin' && password === 'admin') {
+//         this.router.navigate(['/lottie']);
+//         // if(this.showTour = true){
+//         //   // abrir o componente modalTour
+//         //   this.router.navigate(['/tour']);
+//         // } else {
+//         //   this.router.navigate(['/home']);
+//         // }
+//       } else {
+//         this.errorMessage = 'Credenciais inválidas.';
+//       }
+//     }
+//   }
+// }
+
 export class LoginComponent {
   cpf: string = '';
   password: string = '';
@@ -29,8 +67,6 @@ export class LoginComponent {
 
   loginUser(): void {
     const loginSuccess = this.userService.login(this.cpf, this.password);
-
-
     if (loginSuccess) {
       // Redirecionar para a página de sucesso ou realizar ações desejadas
       console.log('Redirecionando para a página de sucesso...');
@@ -42,3 +78,7 @@ export class LoginComponent {
   }
 
 }
+
+
+
+
