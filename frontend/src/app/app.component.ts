486fc4c77bd84cd7b0e1874f5b1771db4f1d4c4e
AlineFernandaDev <alinefds.vieira@gmail.com>
2024-03-14 23:23:12 -0300

    feat: New pages and components for payments

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from "./dashboard/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ModalTourComponent } from './components/modal-tour/modal-tour.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, FormsModule, RouterOutlet, HomeComponent, FooterComponent, LandingPageComponent, ModalTourComponent]
+  imports: [CommonModule, FormsModule, RouterOutlet, HomeComponent, FooterComponent, LandingPageComponent, ModalTourComponent, NavbarComponent, LayoutComponent],
 })
 export class AppComponent {
   title = 'StarPay';
