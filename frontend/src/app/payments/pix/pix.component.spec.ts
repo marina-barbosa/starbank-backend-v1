22f2a7d03eaa40ff433fb9a1ac39e6c0df1fcbf7
Aline Fernanda <alinefds.vieira@gmail.com>
2024-03-15 22:48:36 -0300

    feat: new pages for payments

--- frontend/src/app/payments/pix/pix.component.spec.ts
+++ frontend/src/app/payments/pix/pix.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LayoutComponent } from './layout.component';
+import { PixComponent } from './pix.component';
 
-describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
+describe('PixComponent', () => {
+  let component: PixComponent;
+  let fixture: ComponentFixture<PixComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LayoutComponent]
+      imports: [PixComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LayoutComponent);
+    fixture = TestBed.createComponent(PixComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
