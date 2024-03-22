0ca85f60646f3e4b4734cc7ef6b77b75ea3e75ac
AlineFernandaDev <alinefds.vieira@gmail.com>
2024-03-22 02:32:29 -0300

    fix: Replace files

--- backend/Controllers/Customer.Controller.cs
+++
@@ -1,7 +0,0 @@
-namespace StarPay.backend.Controllers;
-[ApiController]
-[Route("[/Customer]")]
-public class CustomerController : ControllerBase
-{
-
-}
--- backend/Domain/Models/Login.cs
+++
@@ -1,5 +0,0 @@
-public class Login
-{
-    public string CpfCnpj { get; set; }
-    public string Password { get; set; }
-}
